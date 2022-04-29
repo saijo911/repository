c3a2bac1ea7fecc0cb79fa617d3f33393bdee120
saijo911 <shota.saijo.7g@stu.hosei.ac.jp>
2022-04-29 15:15:14 +0900

    my modification

--- test.py
+++ test.py
@@ -101,6 +101,6 @@ def main():
 
     print("==================================================")
     print("===> Finished !")
-
+    print("===> Hello world")
 if __name__ == '__main__':
     main()
